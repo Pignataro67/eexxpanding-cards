7764722f1b80bd7c04cedc4224fd9188c5c8c4ea
Pignataro67 <Pignataro67@gmail.com>
2025-04-22 12:56:24 -0700

    css

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ textarea.addEventListener('keyup', (e) => {
         randomSelect()
     }
 })
+
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+
+    if(e.key === 'Enter') {
+        setTimeout(() => {})
